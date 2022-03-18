2f1eb77b4d524e30dce8b17c9f2bf1ca01386446
Dedygans <dedyharahapchannel@gmail.com>
2022-03-18 14:26:26 +0800

    first commit

---
+++ src/javaapplication8/HaloGeneric.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+/**
+ * HaloGeneric
+ */
+public class HaloGeneric {
+
+    public static void SapaUser(String name) {
+        System.out.printf("Halo user %s",name);
+    }
+
+    public static void main(String[] args) {
+        SapaUser("dedy syahputra rizki harahap");
+    }
+}
